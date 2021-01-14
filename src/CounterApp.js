a09acaf4d3e207b2606839db4374a4337a7fc946
edox61 <63751822+edox61@users.noreply.github.com>
2021-01-14 20:54:28 -0300

    Actualiza funcion incre

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -3,13 +3,13 @@ import React, { useState } from "react";
 const CounterApp = () => {
   const [counter, setCounter] = useState(0);
 
-  const incre = (counter) => {
+  const incre = () => {
     setCounter(counter + 1);
   };
   return (
     <div className="App">
       <h1>Clicks {counter} </h1>
-      <button onClick={() => incre(counter)}> Incrementar</button>
+      <button onClick={incre}> Incrementar</button>
     </div>
   );
 };
